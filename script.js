85f458ef02b67f3b12d278972bc795591c6d5811
Satish Vatturu <vatturu.srinagasatish@gmail.com>
2023-10-31 05:36:03 +0530

    Designed the cells

---
+++ script.js
@@ -0,0 +1,55 @@
+let columns = 26;
+let rows = 100;
+const headerContainer = document.querySelector(".header");
+const serialNumbersContainer = document.querySelector(".sno");
+const mainContainer = document.querySelector(".main");
+
+function createHeaderCells() {
+    for(let i=0;i<=columns;i++)
+    {
+        const headerCell = document.createElement("div");
+        headerCell.className = "header-cell cell";
+        if(i != 0)
+        {
+            headerCell.innerText = String.fromCharCode(64+i);
+        }
+        headerContainer.appendChild(headerCell);
+    }
+}
+
+function createSerialNumberCells() {
+    for(let i=1;i<=rows;i++)
+    {
+        const snoCell = document.createElement("div");
+        snoCell.innerText = i;
+        snoCell.className = "sno-cell cell";
+        serialNumbersContainer.appendChild(snoCell);
+    }
+}
+
+function createRow() {
+    // creates a row and each row will have 26 columns
+    const row = document.createElement("div");
+    row.className = "row";
+    for(let i=1; i<=columns;i++)
+    {
+        const cell = document.createElement("div");
+        cell.className = "main-cell cell";
+        cell.contentEditable = true;
+        row.appendChild(cell);
+    }
+    mainContainer.appendChild(row);
+}
+
+function buildMainSection() {
+    // loop for 100 times
+    for(let i=0;i<rows;i++)
+    {
+        createRow();
+    }
+}
+
+createHeaderCells();
+createSerialNumberCells();
+
+buildMainSection();
